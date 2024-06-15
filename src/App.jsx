2956cfe65e7852d3f5bd6549f28eebfa082f5273
tvxxd <tavdgiridzenika1@gmail.com>
2024-06-15 22:20:08 +0400

    useState for every component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,71 @@
 import { useState } from "react";
 
 function App() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [jobtitle, setJobTitle] = useState("");
+
+  const [university, setUniversity] = useState("");
+  const [fieldofstudy, setFieldOfStudy] = useState("");
+  const [location, setLocation] = useState("");
+
+  const [companyname, setCompanyName] = useState("");
+  const [positiontitle, setPositionTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [language, setLanguage] = useState("");
+  const [tools, setTools] = useState("");
+  const [frameworks, setFrameworks] = useState("");
+
   return (
     <div className="app">
-      <LeftSide />
-      <Preview />
+      <LeftSide
+        setFullname={setFullname}
+        setEmail={setEmail}
+        setPhoneNumber={setPhoneNumber}
+        setAddress={setAddress}
+        setJobTitle={setJobTitle}
+        setUniversity={setUniversity}
+        setFieldOfStudy={setFieldOfStudy}
+        setLocation={setLocation}
+        setCompanyName={setCompanyName}
+        setPositionTitle={setPositionTitle}
+        setDescription={setDescription}
+        setLanguage={setLanguage}
+        setTools={setTools}
+        setFrameworks={setFrameworks}
+      />
+      <Preview
+        fullname={fullname}
+        email={email}
+        phoneNumber={phoneNumber}
+        address={address}
+        jobtitle={jobtitle}
+        university={university}
+        location={location}
+        fieldofstudy={fieldofstudy}
+        companyname={companyname}
+        positiontitle={positiontitle}
+        description={description}
+        language={language}
+        tools={tools}
+        frameworks={frameworks}
+      />
     </div>
   );
 }
 
-function InputField({ labelText, placeholder }) {
+function InputField({ labelText, placeholder, setState }) {
   return (
     <div className="input-field">
       <label>{labelText}</label>
-      <input placeholder={placeholder} type="text" />
+      <input
+        onChange={(e) => setState(e.target.value)}
+        placeholder={placeholder}
+        type="text"
+      />
     </div>
   );
 }
@@ -27,7 +79,13 @@ function DisplayMore({ children, onToggle, showMore }) {
   );
 }
 
-function PersonalDetails() {
+function PersonalDetails({
+  setFullname,
+  setEmail,
+  setPhoneNumber,
+  setAddress,
+  setJobTitle,
+}) {
   const [showMore, setShowMore] = useState(true);
 
   function handleToggle() {
@@ -40,19 +98,38 @@ function PersonalDetails() {
       </DisplayMore>
       {showMore && (
         <>
-          <InputField placeholder="Full name" labelText="Full name" />
-          <InputField placeholder="Email" labelText="Email" />
-          <InputField placeholder="Phone number" labelText="Phone number" />
-          <InputField placeholder="Address" labelText="Address" />
-          <InputField placeholder="Job title" labelText="Job title" />
-          <InputField placeholder="Email" labelText="Email" />
+          <InputField
+            setState={setFullname}
+            placeholder="Full name"
+            labelText="Full name"
+          />
+          <InputField
+            setState={setEmail}
+            placeholder="Email"
+            labelText="Email"
+          />
+          <InputField
+            setState={setPhoneNumber}
+            placeholder="Phone number"
+            labelText="Phone number"
+          />
+          <InputField
+            setState={setAddress}
+            placeholder="Address"
+            labelText="Address"
+          />
+          <InputField
+            setState={setJobTitle}
+            placeholder="Job title"
+            labelText="Job title"
+          />
         </>
       )}
     </div>
   );
 }
 
-function Education() {
+function Education({ setUniversity, setFieldOfStudy, setLocation }) {
   const [showMore, setShowMore] = useState(false);
   function handleToggle() {
     setShowMore((prev) => !prev);
@@ -60,19 +137,33 @@ function Education() {
 
   return (
     <div className="education">
-      <DisplayMore showMore={showMore} onToggle={handleToggle}>Education</DisplayMore>
+      <DisplayMore showMore={showMore} onToggle={handleToggle}>
+        Education
+      </DisplayMore>
       {showMore && (
         <>
-          <InputField placeholder="University" labelText="University" />
-          <InputField placeholder="Location" labelText="Location" />
-          <InputField placeholder="Field of Study" labelText="Field of Study" />
+          <InputField
+            setState={setUniversity}
+            placeholder="University"
+            labelText="University"
+          />
+          <InputField
+            setState={setFieldOfStudy}
+            placeholder="Location"
+            labelText="Location"
+          />
+          <InputField
+            setState={setLocation}
+            placeholder="Field of Study"
+            labelText="Field of Study"
+          />
         </>
       )}
     </div>
   );
 }
 
-function Experience() {
+function Experience({ setCompanyName, setPositionTitle, setDescription }) {
   const [showMore, setShowMore] = useState(false);
 
   function handleToggle() {
@@ -80,19 +171,33 @@ function Experience() {
   }
   return (
     <div className="experience">
-      <DisplayMore showMore={showMore} onToggle={handleToggle}>Experience</DisplayMore>
+      <DisplayMore showMore={showMore} onToggle={handleToggle}>
+        Experience
+      </DisplayMore>
       {showMore && (
         <>
-          <InputField placeholder="Company name" labelText="Company name" />
-          <InputField placeholder="Position title" labelText="Position title" />
-          <InputField placeholder="Description" labelText="Description" />
+          <InputField
+            setState={setCompanyName}
+            placeholder="Company name"
+            labelText="Company name"
+          />
+          <InputField
+            setState={setPositionTitle}
+            placeholder="Position title"
+            labelText="Position title"
+          />
+          <InputField
+            setState={setDescription}
+            placeholder="Description"
+            labelText="Description"
+          />
         </>
       )}
     </div>
   );
 }
 
-function Skills() {
+function Skills({ setLanguage, setTools, setFrameworks }) {
   const [showMore, setShowMore] = useState(false);
 
   function handleToggle() {
@@ -100,76 +205,146 @@ function Skills() {
   }
   return (
     <div className="experience">
-      <DisplayMore showMore={showMore} onToggle={handleToggle}>Skills</DisplayMore>
+      <DisplayMore showMore={showMore} onToggle={handleToggle}>
+        Skills
+      </DisplayMore>
       {showMore && (
         <>
-          <InputField placeholder="Languages" labelText="Languages" />
-          <InputField placeholder="Tools" labelText="Tools" />
-          <InputField placeholder="Frameworks" labelText="Frameworks" />
+          <InputField
+            setState={setLanguage}
+            placeholder="Languages"
+            labelText="Languages"
+          />
+          <InputField
+            setState={setTools}
+            placeholder="Tools"
+            labelText="Tools"
+          />
+          <InputField
+            setState={setFrameworks}
+            placeholder="Frameworks"
+            labelText="Frameworks"
+          />
         </>
       )}
     </div>
   );
 }
 
-function LeftSide() {
+function LeftSide({
+  setFullname,
+  setEmail,
+  setPhoneNumber,
+  setAddress,
+  setJobTitle,
+  setUniversity,
+  setFieldOfStudy,
+  setLocation,
+  setCompanyName,
+  setPositionTitle,
+  setDescription,
+  setLanguage,
+  setTools,
+  setFrameworks,
+}) {
   return (
     <div className="left-side">
-      <PersonalDetails />
-      <Education />
-      <Experience />
-      <Skills />
+      <PersonalDetails
+        setAddress={setAddress}
+        setPhoneNumber={setPhoneNumber}
+        setEmail={setEmail}
+        setFullname={setFullname}
+        setJobTitle={setJobTitle}
+      />
+      <Education
+        setLocation={setLocation}
+        setFieldOfStudy={setFieldOfStudy}
+        setUniversity={setUniversity}
+      />
+      <Experience
+        setDescription={setDescription}
+        setPositionTitle={setPositionTitle}
+        setCompanyName={setCompanyName}
+      />
+      <Skills
+        setFrameworks={setFrameworks}
+        setTools={setTools}
+        setLanguage={setLanguage}
+      />
     </div>
   );
 }
 
-function Preview() {
+function Preview({
+  fullname,
+  email,
+  phoneNumber,
+  address,
+  jobtitle,
+  university,
+  location,
+  fieldofstudy,
+  companyname,
+  positiontitle,
+  description,
+  language,
+  tools,
+  frameworks,
+}) {
   return (
     <div className="preview">
-      <div className="for-personal-details">
-        <h3>Test Tavdgiridze</h3>
-        <span>Software Developer</span>
-        <p>gmail213123@gmail.com</p>
-        <p>39248324923</p>
-        <p>Tbilisi,Georgia</p>
-      </div>
-      <div className="for-education">
-        <h4>Education</h4>
-        <div className="e-details">
-          <p>Caucasus University</p>
-          <p>Tbilisi, Georgia</p>
+      {(fullname || email || phoneNumber || address || jobtitle) && (
+        <div className="for-personal-details">
+          {fullname && <h3>{fullname}</h3>}
+          {jobtitle && <span>{jobtitle}</span>}
+          {email && <p>{email}</p>}
+          {phoneNumber && <p>{phoneNumber}</p>}
+          {address && <p>{address}</p>}
         </div>
-        <p>Computer Science, Bachelors</p>
-        <p></p>
-      </div>
-      <div className="for-experience">
-        <h4>Experience</h4>
-        <span>companyname | fullstack web developer</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-          delectus recusandae ipsum nesciunt rerum est, labore repellendus
-          beatae animi nam molestiae vel eveniet fugiat debitis cupiditate velit
-          explicabo dolorum quisquam.
-        </p>
-      </div>
-      <div className="for-skills">
-        <h4>Skills</h4>
-        <div className="flex">
-          <span>Programming Languages:</span>
-          <span>Javascript</span>
+      )}
+      {(university || location || fieldofstudy) && (
+        <div className="for-education">
+          <h4>Education</h4>
+          <div className="e-details">
+            {university && <p>{university}</p>}
+            {location && <p>{location}</p>}
+          </div>
+          {fieldofstudy && <p>{fieldofstudy}</p>}
         </div>
-
-        <div className="flex">
-          <span>Frameworks:</span>
-          <span>ReactJS</span>
+      )}
+      {(companyname || positiontitle || description) && (
+        <div className="for-experience">
+          <h4>Experience</h4>
+          {companyname && <span>{companyname}</span>}
+          {positiontitle && <span> | {positiontitle}</span>}
+          {description && <p>{description}</p>}
         </div>
-
-        <div className="flex">
-          <span>Tools:</span>
-          <span>Git</span>
+      )}
+      {(language || frameworks || tools) && (
+        <div className="for-skills">
+          <h4>Skills</h4>
+          {language && (
+            <div className="flex">
+              <span>Programming Languages:</span>
+              <span>{language}</span>
+            </div>
+          )}
+          {frameworks && (
+            <div className="flex">
+              <span>Frameworks:</span>
+              <span>{frameworks}</span>
+            </div>
+          )}
+          {tools && (
+            <div className="flex">
+              <span>Tools:</span>
+              <span>{tools}</span>
+            </div>
+          )}
         </div>
-      </div>
+      )}
     </div>
   );
 }
+
 export default App;
